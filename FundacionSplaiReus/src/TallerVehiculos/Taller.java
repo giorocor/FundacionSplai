29156f423faa79a7bcbd9ce4a5dcd38207421291
giorocor <gio-giovanny@hotmail.com>
2021-02-11 17:06:23 +0200

    Ejercicios herencia y objetos

---
+++ FundacionSplaiReus/src/TallerVehiculos/Taller.java
@@ -0,0 +1,10 @@
+package TallerVehiculos;
+
+public class Taller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FundacionSplaiReus/src/TallerVehiculos/Vehiculos.java
@@ -0,0 +1,5 @@
+package TallerVehiculos;
+
+public class Vehiculos {
+
+}
